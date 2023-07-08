1ecbb4be32ed9c94529416f8da0ccb14b40d9cd7
Mr-Michael-dev <michael.oyedepo@gmail.com>
2023-07-08 17:07:12 +0100

    added a function to clear storage when going start

--- JS/closeWelcome.js
+++ JS/closeWelcome.js
@@ -34,3 +34,4 @@ const validateForm = () => {
     return true;
   }
 };
+
---
+++ JS/goHome.js
@@ -0,0 +1,13 @@
+
+home.addEventListener("click", (e) => {
+  e.preventDefault(); // Prevent default link behavior
+
+  // Show confirmation dialog and check if the user wants to exit
+  const confirmExit = confirm("This will clear all existing tasks. Are you sure you want to exit?");
+
+  if (confirmExit) {
+    sessionStorage.clear();
+    localStorage.clear();
+    window.location.href = "index.html";
+  }
+});
